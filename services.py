3aefe2495f0c2e9dbaaf53495954a514b2720500
fractivalk <nickphan@vt.edu>
2017-11-26 10:17:52 -0500

    example to test

---
+++ services.py
@@ -0,0 +1,13 @@
+import pymongo
+from flask import Flask
+
+#online example, creates server on ip and returns Hello World
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host = "192.168.137.182', port=5000", debug=True) #IP is based of current pi being used, 5000 is Flask DP
