e385cbb3e4a4b75332508111d911a61860e751f2
fractivalk <nickphan@vt.edu>
2017-11-26 16:56:08 -0500

    added POST request handler

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, jsonify
 from pymongo import MongoClient
+from flask import make_response
 
 #online example, creates server on ip and returns Hello World
 
@@ -28,6 +29,17 @@ example1 = [
 @app.route("/example1", methods =['GET'])
 def get_example1():
     return jsonify({'example1': example1})
+
+@app.route("/example1", methods = ['POST'])
+def create_example1():
+    example = {
+        'id' : example1[-1]['id'] + 1,
+        'name': request.json['name'],
+        'level': request.json.get('level', "")
+    }
+    example1.append(example)
+    return jsonify({'example': example}), 201
+
 
 if __name__ == "__main__":
     app.run(host = "192.168.137.182", port=5000, debug=True) #IP is based of current pi being used, 5000 is Flask DP
