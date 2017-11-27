75de85d58d2b0e0b2b1e85a65e7242b85fc7f469
= <=>
2017-11-27 18:13:41 -0500

    testing

--- led.py
+++ led.py
@@ -2,7 +2,7 @@ import time
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -20,34 +20,35 @@ RED = GPIO.PWM(red, Freq)
 GREEN = GPIO.PWM(green, Freq)
 BLUE = GPIO.PWM(blue, Freq)
 
-RED.start(50)
-GREEN.start(10)
-BLUE.start(30)
+while 1:
+    RED.start(50)
+    GREEN.start(10)
+    BLUE.start(30)
 
-@app.route("/")
-def main():
-   # return render_template('main.html')
-   return "Hello World!"
-
-@app.route("/LED")
-def LED():
-    status =    request.args.get('status', default = '', type = str)
-    color =     request.args.get('color', default = '', type = str)
-    intensity = request.args.get('intensity', default = 0, type = int)
-    if status == "on":
-        GREEN.ChangeDutyCycle(0)
-        RED.ChangeDutyCycle(0)
-        BLUE.ChangeDutyCycle(0)
-        if color == 'red':
-            RED.ChangeDutyCycle(intensity)
-        elif color == 'green':
-            GREEN.ChangeDutyCycle(intensity)
-        elif color == 'blue':
-            BLUE.ChangeDutyCycle(intensity)
-    if status == "off":
-        GREEN.ChangeDutyCycle(0)
-        RED.ChangeDutyCycle(0)
-        BLUE.ChangeDutyCycle(0)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+# @app.route("/")
+# def main():
+#    # return render_template('main.html')
+#    return "Hello World!"
+#
+# @app.route("/LED")
+# def LED():
+#     status =    request.args.get('status', default = '', type = str)
+#     color =     request.args.get('color', default = '', type = str)
+#     intensity = request.args.get('intensity', default = 0, type = int)
+#     if status == "on":
+#         GREEN.ChangeDutyCycle(0)
+#         RED.ChangeDutyCycle(0)
+#         BLUE.ChangeDutyCycle(0)
+#         if color == 'red':
+#             RED.ChangeDutyCycle(intensity)
+#         elif color == 'green':
+#             GREEN.ChangeDutyCycle(intensity)
+#         elif color == 'blue':
+#             BLUE.ChangeDutyCycle(intensity)
+#     if status == "off":
+#         GREEN.ChangeDutyCycle(0)
+#         RED.ChangeDutyCycle(0)
+#         BLUE.ChangeDutyCycle(0)
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80, debug=True)
