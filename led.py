a3aed4f16614cfbe3e946c461f1d6dbb3b05ac81
= <=>
2017-11-27 18:27:26 -0500

    more tests

--- led.py
+++ led.py
@@ -7,9 +7,9 @@ from flask import Flask, render_template, request
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-red = 4
-green = 17
-blue = 27
+red = 23
+green = 18
+blue = 4
 
 GPIO.setup(red, GPIO.OUT)
 GPIO.setup(green, GPIO.OUT)
@@ -19,13 +19,28 @@ Freq = 100
 RED = GPIO.PWM(red, Freq)
 GREEN = GPIO.PWM(green, Freq)
 BLUE = GPIO.PWM(blue, Freq)
-print('testing1')
-while 1:
-    print('testing2')
-    RED.start(100)
-    GREEN.start(100)
-    BLUE.start(100)
 
+RED.start(0)
+GREEN.start(0)
+BLUE.start(0)
+
+print('testing1')
+
+def color(R, G, B, on_time):
+    # Color brightness range is 0-100%
+    RED.ChangeDutyCycle(R)
+    GREEN.ChangeDutyCycle(G)
+    BLUE.ChangeDutyCycle(B)
+    time.sleep(on_time)
+
+    # Turn all LEDs off after on_time seconds
+    RED.ChangeDutyCycle(0)
+    GREEN.ChangeDutyCycle(0)
+    BLUE.ChangeDutyCycle(0)
+    time.sleep(on_time)
+
+while 1:
+    color(50, 10, 30, 1)
 # @app.route("/")
 # def main():
 #    # return render_template('main.html')
