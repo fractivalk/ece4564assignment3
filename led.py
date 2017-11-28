07192c9fe4c74c83d6cf8a86187f7161d1af7411
= <=>
2017-11-27 20:44:37 -0500

    tests

--- led.py
+++ led.py
@@ -1,6 +1,7 @@
 import time
 import RPi.GPIO as GPIO
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, advertise
+import json
 
 app = Flask(__name__)
 
@@ -24,6 +25,9 @@ RED.start(0)
 GREEN.start(0)
 BLUE.start(0)
 
+LED_status = False
+LED_color = 'red'
+
 # def color(R, G, B, on_time):
 #     # Color brightness range is 0-100%
 #     RED.ChangeDutyCycle(R)
@@ -40,30 +44,49 @@ BLUE.start(0)
 # while 1:
 #     color(50, 10, 30, 1)
 
+
 @app.route("/")
 def main():
    # return render_template('main.html')
    return "Hello World!"
 
-@app.route("/LED")
+@advertise(status=["on", "off"],colors=["red","green","blue","magenta","cyan","yellow","white"],intensity='1-100')
+@app.route("/LED", methods=['GET', 'POST'])
 def LED():
-    status =    request.args.get('status', default = '', type = str)
-    color =     request.args.get('color', default = '', type = str)
-    intensity = request.args.get('intensity', default = 0, type = int)
-    if status == "on":
-        GREEN.ChangeDutyCycle(0)
-        RED.ChangeDutyCycle(0)
-        BLUE.ChangeDutyCycle(0)
-        if color == 'red':
-            RED.ChangeDutyCycle(intensity)
-        elif color == 'green':
-            GREEN.ChangeDutyCycle(intensity)
-        elif color == 'blue':
-            BLUE.ChangeDutyCycle(intensity)
-    if status == "off":
-        GREEN.ChangeDutyCycle(0)
-        RED.ChangeDutyCycle(0)
-        BLUE.ChangeDutyCycle(0)
+    if (request.method == 'POST'):
+        data = json.loads(request.data)
+        LED_color = data['color']
+        LED_status = data['status']
+        if LED_status == 'on':
+            GREEN.ChangeDutyCycle(0)
+            RED.ChangeDutyCycle(0)
+            BLUE.ChangeDutyCycle(0)
+            if LED_color == 'red':
+                RED.ChangeDutyCycle(data['intensity'])
+            elif LED_color == 'green':
+                GREEN.ChangeDutyCycle(data['intensity'])
+            elif LED_color == 'blue':
+                BLUE.ChangeDutyCycle(data['intensity'])
+            elif LED_color == 'magenta':
+                RED.ChangeDutyCycle(data['intensity'])
+                BLUE.ChangeDutyCycle(data['intensity'])
+            elif LED_color == 'cyan':
+                GREEN.ChangeDutyCycle(data['intensity'])
+                BLUE.ChangeDutyCycle(data['intensity'])
+            elif LED_color == 'yellow':
+                RED.ChangeDutyCycle(data['intensity'])
+                GREEN.ChangeDutyCycle(data['intensity'])
+            elif LED_color == 'white':
+                RED.ChangeDutyCycle(data['intensity'])
+                GREEN.ChangeDutyCycle(data['intensity'])
+                BLUE.ChangeDutyCycle(data['intensity'])
+        elif LED_status == 'off':
+            GREEN.ChangeDutyCycle(0)
+            RED.ChangeDutyCycle(0)
+            BLUE.ChangeDutyCycle(0)
+        return 'Changed LED'
+    else:
+        return ('LED status: ' + LED_status + '\nLED color: ' + LED_color)
 
 if __name__ == "__main__":
     app.run(host='172.29.33.66', port=5000, debug=True)
