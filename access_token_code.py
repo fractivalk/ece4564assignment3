96bdd6331f5544c63829f3d3cfc669e1216b19e1
Eric Walters <ericjw@vt.edu>
2017-11-30 15:55:55 -0500

    access code

---
+++ access_token_code.py
@@ -0,0 +1,3 @@
+# Access token and API url
+api_url = 'https://vt.instructure.com/api/v1/groups/46547/files/'
+auth = {'access_token':'4511~N31cAx15APjv7QDgwYtY0jIyv7R8bBetGgJFIn0kql3cb4IQjpOyVKPgctxn813C'}
