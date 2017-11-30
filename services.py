38cd3273d742f2330b07267751775d342c26fe43
Eric Walters <ericjw@vt.edu>
2017-11-30 16:16:11 -0500

    changed upload/download path and params

--- services.py
+++ services.py
@@ -96,6 +96,49 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
+@app.route("/", methods =['GET'])
+@requires_auth
+def get_example1():
+    return "This is the root directory. Why are you here?"
+
+@app.route("/Canvas", methods=['GET', 'POST'])
+@requires_auth
+def canvas():
+    file = request.args.get('file')
+    if request.args.get('operation') == 'upload':
+        # Set up a session
+        session = requests.Session()
+        session.headers = {'Authorization': 'Bearer %s' % auth['access_token']}
+
+        # Step 1 - tell Canvas you want to upload a file
+        payload = {}
+        payload['name'] = file
+        payload['parent_folder_path'] = '/'
+        r = session.post(api_url, data=payload)
+        r.raise_for_status()
+        r = r.json()
+
+        # Step 2 - upload file
+        payload = list(r['upload_params'].items()) # Note this is now a list of tuples
+        with open(file, 'rb') as f:
+            file_content = f.read()
+        payload.append((u'file', file_content)) # Append file at the end of list of tuples
+        r = requests.post(r['upload_url'], files=payload)
+        r.raise_for_status()
+        r = r.json()
+        return "File \'{}\' successfully posted\n".format(file
+        
+    elif request.args.get('operation') == 'download':
+        id = ''
+        for i in requests.get(api_url, params=auth).json():
+            if i['filename'] == file:
+                id = i['id']
+        if id == '':
+            return 'File \'{}\' not present in remote directory\n'.format(file)
+        urllib.request.urlretrieve(requests.get('{}/{}'.format(api_url, id) , params=auth).json()['url'], download_file)
+        return 'File \'{}\' downloaded successfully\n'.format(file)
+    
+''' 
 #Get command handling python http method to download from canvas API at group directory
 @app.route("/download/<download_file>", methods=['GET'])
 @requires_auth
@@ -108,11 +151,6 @@ def get_download(download_file):
         return 'File \'{}\' not present in remote directory\n'.format(download_file)
     urllib.request.urlretrieve(requests.get('{}/{}'.format(api_url, id) , params=auth).json()['url'], download_file)
     return 'File \'{}\' downloaded successfully\n'.format(download_file)
-
-@app.route("/", methods =['GET'])
-@requires_auth
-def get_example1():
-    return "This is the root directory. Why are you here?"
 
 @app.route("/upload/<upload_file>", methods =['POST'])
 @requires_auth
@@ -139,6 +177,7 @@ def create_upload(upload_file):
     r = r.json()
 
     return "File \'{}\' successfully posted\n".format(upload_file)
+'''
 
 #custom API functionality
 
