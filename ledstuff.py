cc61bcd8b49ce3c89b94a494fd0a90a4d6d34947
= <=>
2017-11-29 20:36:47 -0500

    gpio cleanup

--- ledstuff.py
+++ ledstuff.py
@@ -37,6 +37,11 @@ def color(R, G, B, on_time):
     GREEN.ChangeDutyCycle(0)
     BLUE.ChangeDutyCycle(0)
     time.sleep(on_time)
+try:
+    while 1:
+        color(50, 10, 30, 1)
+except KeyboardInterrupt:
+    pass
 
-while 1:
-    color(50, 10, 30, 1)
+p.stop()
+GPIO.cleanup()
